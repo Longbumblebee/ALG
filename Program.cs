36b92649a48ef508a27aec0c0e571d00445aec2d
Longbumblebee <79219705+Longbumblebee@users.noreply.github.com>
2021-12-08 23:24:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,40 @@
 ﻿using System;
 
-namespace ALGHomework_1_1
+namespace ALGHomework_1_2
 {
     class Program
     {
-       
-            public class TestCase
-            {
-                public int Number { get; set; }
-                public string Expected { get; set; }
-                public Exception ExpectedException { get; set; }
-            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;                                            // O(1)
 
-            static void Test(TestCase testCase)
+            for (int i = 0; i < inputArray.Length; i++)             // O(inputArray.Length)
             {
-                try
+                for (int j = 0; j < inputArray.Length; j++)         // O(inputArray.Length)
                 {
-                    string checkResult = CheckSimple(testCase.Number);
-                    if (checkResult == testCase.Expected)
+                    for (int k = 0; k < inputArray.Length; k++)     // O(inputArray.Length)
                     {
-                        Console.WriteLine("VALID TEST");
-                    }
-                    else
-                    {
-                        Console.WriteLine("NOT VALID TEST");
+                        int y = 0;                                  // O(inputArray.Length)
+
+                        if (j != 0)                                 // O(inputArray.Length)
+                        {
+                            y = k / j;                              // O(inputArray.Length)
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;       // O(inputArray.Length)
                     }
                 }
-                catch (Exception ex)
-                {
-                    if (testCase.ExpectedException != null)
-                    {
-                        Console.WriteLine("VALID TEST");
-                    }
-                    else
-                    {
-                        Console.WriteLine("NOT VALID TEST");
-                    }
-
-                }
-
             }
 
-            static void Main(string[] args)
-            {
-                TestCase testCase1 = new TestCase()
-                {
-                    Number = 11,
-                    Expected = "Простое",
-                    ExpectedException = null
-                };
+            return sum;                                             // O(1)
 
-                TestCase testCase2 = new TestCase()
-                {
-                    Number = 24,
-                    Expected = "Не простое",
-                    ExpectedException = null
-                };
-
-                TestCase testCase3 = new TestCase()
-                {
-                    Number = 6781,
-                    Expected = "Простое",
-                    ExpectedException = null
-                };
-
-
-                TestCase testCase4 = new TestCase()
-                {
-                    Number = -5,
-                    Expected = "",
-                    ExpectedException = new Exception()
-                };
-
-                TestCase testCase5 = new TestCase()
-                {
-                    Number = 9,
-                    Expected = "Простое",
-                    ExpectedException = null
-                };
-
-                Test(testCase1);
-                Test(testCase2);
-                Test(testCase3);
-                Test(testCase4);
-                Test(testCase5);
-                Console.ReadKey();
-            }
-
-            static string CheckSimple(int number)
-            {
-                int d = 0;
-                int i = 2;
-                while (i < number)
-                {
-                    if (number % i == 0)
-                        d++;
-                    i++;
-                }
-
-                if (d == 0)
-                    return "Простое";
-                else
-                    return "Не простое";
-            }
+            // производительность алгоритма составит:
+            // O(2) + O(inputArray.Length*inputArray.Length*4*inputArray.Length) + O(inputArray.Length) =
+            // O(4*Pow(inputArray.Length,3)) = O(Pow(inputArray.Length,3))
         }
     }
-
+}
